78191e17bf61f36dc8c108aee858c6215869c752
David Orji <davidorji93@gmail.com>
2024-11-13 23:36:54 +0100

    fix dependency

--- app/components/ApplicationStats.tsx
+++ app/components/ApplicationStats.tsx
@@ -2,8 +2,15 @@
 
 import { Pie } from 'react-chartjs-2';
 
+// Define the expected type for `stats`
+interface StatsProps {
+  accepted: number;
+  pending: number;
+  rejected: number;
+}
 
-const ApplicationStats = ({ stats }) => {
+// Specify `stats` as the expected prop type for the component
+const ApplicationStats = ({ stats }: { stats: StatsProps }) => {
   const data = {
     labels: ['Accepted', 'Pending', 'Rejected'],
     datasets: [
